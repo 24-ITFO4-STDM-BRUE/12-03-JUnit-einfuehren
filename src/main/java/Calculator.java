3e8796a2018c0818abecf901e18931133b394b71
Peter <brueskep@lebk-muenster.de>
2024-09-13 08:43:36 +0200

    Füge JUnit und erste Tests hinzu

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -11,10 +11,10 @@ public class Calculator {
         return a * b;
     }
 
-    public double divide(int a, int b) {
+    public int divide(int a, int b) {
         if (b == 0) {
             throw new IllegalArgumentException("Cannot divide by zero.");
         }
-        return (double) a / b;
+        return  a / b;
     }
 }
