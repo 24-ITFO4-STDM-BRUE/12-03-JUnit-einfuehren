592ec5ffbc6bf296bb9d59e612ff3b673eb09cd8
lenzenvalentin <lenzenvalentin@lebk247.de>
2024-09-23 10:28:36 +0200

    Added testDivideZero()

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,9 +1,22 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.time.Duration;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
-    private final Calculator calculator = new Calculator();
+    private Calculator calculator = new Calculator();
+
+    @BeforeEach
+    public void createNewCalculator() {
+        calculator = new Calculator();
+    }
+
+    @AfterEach
+    public void afterTest() {
+        System.out.println("Test abgeschlossen.");
+    }
 
     @Test
     public void testAdd() {
@@ -25,4 +38,19 @@ public class CalculatorTest {
 
     @Test
     public void testDivideZero() { assertThrows(IllegalArgumentException.class, () -> calculator.divide(2, 0), "Teilen durch 0 sollte eine IllegalArgumentException schmeißen."); }
+
+    @Test
+    public void testFibonacciNotNull() { assertNotNull(calculator.generateFibonacci(5), "generateFibonacci darf bei Zahlen > 0 nicht null zurückgeben."); }
+
+    @Test
+    public void testFibonacciZero() { assertArrayEquals(new int[]{}, calculator.generateFibonacci(0), "generateFibonacci soll bei 0 ein leeres Array zurückgeben."); }
+
+    @Test
+    public void testFibonacciFirstTwo() { assertArrayEquals(new int[]{ 0, 1 }, calculator.generateFibonacci(2), "Die Zahlenreihe muss mit 0, 1 beginnen."); }
+
+    @Test
+    public void testFibonacci() { assertArrayEquals(new int[]{ 0, 1, 1, 2, 3 }, calculator.generateFibonacci(5), "Die Fibonacci-Reihe der Länge 5 muss so aussehen: [0, 1, 1, 2, 3]."); }
+
+    @Test
+    public void testFibonacciTimeout() { assertTimeout(Duration.ofSeconds(1), () -> calculator.generateFibonacci(100000000), "Die Generierung der Fibonacci-Reihe sollte nicht länger als eine Sekunde dauern."); }
 }
