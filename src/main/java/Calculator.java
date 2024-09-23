858bb59ed2c360c28b68785ca2f7570dc3c8d931
zunidzaPatrick <zunidzapatrick@lebk247.de>
2024-09-23 10:59:38 +0200

    test(CalculatorTest): Add testGenerateFibonacci_

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -17,4 +17,19 @@ public class Calculator {
         }
         return  a / b;
     }
+    public int[] generateFibonacci(int n) {
+        if (n <= 0) return new int[]{};
+        int[] fib = new int[n];
+        fib[0] = 0;
+        if (n > 1) {
+            fib[1] = 1;
+            for (int i = 2; i < n; i++) {
+                fib[i] = fib[i - 1] + fib[i - 2];
+            }
+        }
+        return fib;
+    }
+
+
 }
+
