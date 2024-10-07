04d26490b18834371aa31a73b3d7114539603e05
Peter Brüske <BrueskeP@schule.local>
2024-10-07 09:47:49 +0200

    Revert "Add BeforeEach to init Calculator"
    This reverts commit 0305843e1ddce0451d7c6575406a68e48c20752f.

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,15 +1,9 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
-    private Calculator calculator;
-
-    @BeforeEach
-    public void init() {
-        calculator = new Calculator();
-    }
+    private final Calculator calculator = new Calculator();
 
     @Test
     public void testAdd() {
